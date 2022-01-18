d2c9a01100c106c826237b213e86355d97fa4607
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:47:25 +0300

    git commit;

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,10 +12,12 @@ public class Application {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
+            Timer beanTimer = applicationContext.getBean(Timer.class);
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
-            bean.whatAnimalSay();
+            bean.whatAnimalSay(beanTimer);
         }
     }
 
 }
+
